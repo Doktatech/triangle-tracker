40958b16b9fb4738e058e99e81cd6c8996a0dfbf
Doktatech <Doktatech2@gmail.com>
2019-02-15 15:14:51 +0300

    Declare looping conditions with if-else condition

--- scripts.js
+++ scripts.js
@@ -2,9 +2,15 @@ function triangle(){
   var a =parseInt(document.getElementsByName('lengthOne').value);
   var b =parseInt(document.getElementsByName('lengthTwo').value);
   var c =parseInt(document.getElementsByName('lengthThree').value);{
-    if (a===b && b==c) {
-      alert(This is an Equirateral Triangle)
-    } else {if (true) {
+    if (a===b && b===c) {
+      alert(This is an Equirateral Triangle);
+
+    } else {if (a===b || a===c || b===c ) {
+
+      alert(This is an Isosceles Triangle);
+
+    }else {
+      alert (This triangle is Scalene);
 
     }
 
