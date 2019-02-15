5446a374324ce9d79d1f11d5f84f395f635f371c
Doktatech <Doktatech2@gmail.com>
2019-02-15 15:05:58 +0300

    add variable a,b,c and equate to lengthOne, lengthTwo and lengthThree respectively

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function triangle(){
+  var a =parseInt(document.getElementsByName('lengthOne').value);
+  var b =parseInt(document.getElementsByName('lengthTwo').value);
+  var c =parseInt(document.getElementsByName('lengthThree').value);{
+    if (a===b && b==c) {
+      alert(This is an Equirateral Triangle)
+    } else {if (true) {
+
+    }
+
+    }
+  }
+}
